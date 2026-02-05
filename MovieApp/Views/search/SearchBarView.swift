b04df8e4f6f7c1dce4d1f03582a9fe0635df8a66
SriRajeswari-21 <SriRajeswari.P@encora.com>
2026-02-04 23:12:49 -0800

    added emptysearchview searchbarview and empty files of searchmoviecardview searchview

---
+++ MovieApp/Views/search/SearchBarView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBarView.swift
+//  MovieApp
+//
+//  Created by rentamac on 2/4/26.
+//
+
+import Foundation
---
+++ MovieApp/Views/search/SearchView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchView.swift
+//  MovieApp
+//
+//  Created by rentamac on 2/4/26.
+//
+
+import Foundation
