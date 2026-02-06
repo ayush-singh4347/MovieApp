bf92c71cdc01349973f6b12abceecbda64caeddb
ThanujaBalla <thanujaballahyd@gmail.com>
2026-02-05 22:05:43 -0800

    pulled all the changes from develop branch

---
+++ MovieApp/Views/Home/HomeView.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @StateObject private var viewModel = HomeViewModel()
+
+    var body: some View {
+        NavigationStack {
+            List(viewModel.movies) { movie in
+                Text(movie.title)
+            }
+            .navigationTitle("Trending Movies")
+            .onAppear {
+                viewModel.loadDummyData()
+            }
+        }
+    }
+}
