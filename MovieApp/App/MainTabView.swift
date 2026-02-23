9622c934e0126ef65b8515f1dc1a7f5617268cc6
ThanujaBalla <thanujaballahyd@gmail.com>
2026-02-23 03:52:58 -0800

    toll bar changes

--- MovieApp/App/MainTabView.swift
+++ MovieApp/App/MainTabView.swift
@@ -10,7 +10,28 @@ enum Tab {
 struct MainTabView: View {
     @StateObject private var watchlistVM = WatchlistViewModel()
     @State private var selectedTab: Tab = .home
+    init() {
+    let appearance = UITabBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
 
+   
+    appearance.backgroundColor = UIColor.systemBackground
+
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(Color.purple)
+    appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+    .foregroundColor: UIColor.systemPurple
+    ]
+
+    
+    appearance.stackedLayoutAppearance.normal.iconColor = UIColor.gray
+    appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+    .foregroundColor: UIColor.gray
+    ]
+
+    UITabBar.appearance().standardAppearance = appearance
+    UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
     var body: some View {
 
         TabView(selection: $selectedTab) {
@@ -49,7 +70,7 @@ struct MainTabView: View {
                        .tag(Tab.profile)
         }
         .environmentObject(watchlistVM)
-        .tint(.purple)
+        
     }
 }
 
